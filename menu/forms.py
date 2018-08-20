c0c8772334067bc56769db77e0c74272a5dcc5eb
ykuruta <yasukokurata@gmail.com>
2018-08-20 11:36:06 -0400

    added custom validation

--- menu/forms.py
+++ menu/forms.py
@@ -1,7 +1,14 @@
 from django import forms
+from django.core import validators
 from django.forms.extras.widgets import SelectDateWidget
 
 from .models import Menu, Item, Ingredient
+
+
+def honeypot(value):
+    if value:
+        raise forms.ValidationError('is not empty')
+
 
 class MenuForm(forms.ModelForm):
     season = forms.CharField(max_length=20, required=True)
@@ -10,6 +17,12 @@ class MenuForm(forms.ModelForm):
                     help_text='YYYY-MM-DD, MM/DD/YYYY, or MM/DD/YY',
                     required=True
                     )
+    honeypot = forms.CharField(required=False,
+                                widget=forms.HiddenInput,
+                                label="Leave empty",
+                                validators=[honeypot]
+                                )
+
 
     class Meta:
         model = Menu
